18df634d405f1aad8481094962132371f8aff8b2
njl07 <akanass@icloud.com>
2018-10-31 18:04:39 +0100

    feat(example): migrate to angular v7.0.1+ and ng-universal v7.0.0+

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
 import { TransferHttpCacheModule } from '@hapiness/ng-universal-transfer-http';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { APP_ROUTES } from './app.routes';
-import { ClientRequestService } from './shared/services/client-request.service';
-import { REQUEST_SERVICE, LOGGER_SERVICE } from './shared/services/tokens';
 import { MyHttpInterceptor } from './shared/interceptors/my-http.interceptor';
-import { HttpService } from './shared/services/http.service';
 import { ClientLoggerService } from './shared/services/client-logger.service';
+import { ClientRequestService } from './shared/services/client-request.service';
+import { HttpService } from './shared/services/http.service';
+import { LOGGER_SERVICE, REQUEST_SERVICE } from './shared/services/tokens';
 
 @NgModule({
     declarations: [
@@ -25,7 +25,7 @@ import { ClientLoggerService } from './shared/services/client-logger.service';
             headerNameToOverrideUrlInKeyCachingGeneration: 'x-key-client-url'
         }),
         HttpClientModule,
-        APP_ROUTES,
+        AppRoutingModule,
         MatCardModule
     ],
     providers: [
